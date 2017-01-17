e09f47639cd6e082acfedfb4b9635da58c49c8d6
Elena Seredova <e.grishch@gmail.com>
2017-01-17 16:23:25 +0300

    add waitForTimeout option

--- test.spec_tickets-183.js
+++ test.spec_tickets-183.js
@@ -6,8 +6,9 @@ const webdriver = require('webdriverio');
 describe('tickets-183: Экран расписания фильма - поиск', function () {
     let browser;
 
-    before(async function () {
+    before(async function() {
         browser = webdriver.remote({
+            waitforTimeout: '15000',
             desiredCapabilities: {
                 browserName: 'chrome'
             }
@@ -18,14 +19,16 @@ describe('tickets-183: Экран расписания фильма - поиск
         await browser.url('https://widget.tickets.yandex.ru/w/events/50433?regionId=213&clientKey=bb40c7f4-11ee-4f00-9804-18ee56565c87');
     });
 
+
+
     it('should show search field', async function () {
         await browser
-        .waitForVisible('.input_field',15000);
+        .waitForVisible('.input_field');
     });
 
     it('should show searched place', async function () {
         await browser
-        .waitForVisible('.input_field',15000)
+        .waitForVisible('.input_field')
         .setValue('.input_field','Балтика')
         .waitForVisible('.session-item-caption_title')
         .getHTML('.session-item-caption_title', true).then(res => {
@@ -41,7 +44,7 @@ describe('tickets-183: Экран расписания фильма - поиск
     it('should show error message in case place not found', async function () {
         await browser
         .setValue('.input_field',' ')
-        .waitForVisible('.message_title',15000)
+        .waitForVisible('.message_title')
         .getHTML('.message_title', true).then(res => {
             assert(((res.indexOf('Ничего не нашлось') !== -1)), 'Некорректный текст ошибки');
         });
@@ -49,7 +52,7 @@ describe('tickets-183: Экран расписания фильма - поиск
 
     it('should show sessions after filters reset', async function () {
         await browser
-        .waitForVisible('.message_inner .fakelink',15000)
+        .waitForVisible('.message_inner .fakelink')
         .click('.message_inner .fakelink')
         .waitForVisible('.session-item-caption_title');
     });
